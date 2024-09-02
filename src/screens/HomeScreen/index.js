af175bf4ffd2c604e232bcf0a089fdc7d1f32714
gmourar <gabrielmourarocha@gmail.com>
2024-09-02 12:44:59 -0300

    teste - commit

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { VStack, Button, Input, Text } from 'native-base';
 
 export default function HomeScreen() {
+
+  const test = () => {
+    alert("Hello World") 
+  }
   return (
     <VStack space={4} mt={10} justifyContent={'center'} alignItems={'center'} padding={5}>
       <Text fontSize="lg" bold>
         Prova por Indução
       </Text>
       <Input placeholder="Teste Teste Teste" />
-      <Button onPress={() => alert('Teste iniciado')} width={'100%'}>
+      <Button onPress={test} width={'100%'}>
         Iniciar Teste
       </Button>
     </VStack>
